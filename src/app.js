fe8fd4db6876486124a92758fac0e07b034f8a34
Joey Farruggio <joey@joeyfarruggio.com>
2022-02-07 18:01:32 -0600

    WIP

--- src/app.js
+++ src/app.js
@@ -3,3 +3,5 @@ import initAlpine from './initAlpine';
 
 injectCSS();
 initAlpine();
+
+console.log(process.env.NODE_ENV);
--- src/injectCSS.js
+++ src/injectCSS.js
@@ -4,11 +4,7 @@ const injectCSS = () => {
 
   link.type = "text/css";
   link.rel = "stylesheet";
-  if (process.env.NODE_ENV  == 'production') {
-      link.href = "https://raw.githubusercontent.com/joseph-farruggio/Ineject-HTML-Components-with-Script/master/dist/styles.css";
-  } else {
-      link.href = "./../dist/styles.css";
-  }
+  link.href = "./../dist/styles.css";
   head.appendChild(link);
 }
 

50ca2cb1d2f17205f6e8fe93d3a334ea3131fc81
Joey Farruggio <joey@joeyfarruggio.com>
2022-02-12 13:50:52 -0600

    prod CSS URL

--- src/injectCSS.js
+++ src/injectCSS.js
@@ -1,15 +1,16 @@
 const injectCSS = () => {
-  var head = document.head;
-  var link = document.createElement("link");
+	var head = document.head;
+	var link = document.createElement("link");
 
-  link.type = "text/css";
-  link.rel = "stylesheet";
-  if (process.env.NODE_ENV  == 'production') {
-    link.href = "https://raw.githack.com/joseph-farruggio/Ineject-HTML-Components-with-Script/master-built/styles.css";
-  } else {
-    link.href = "./../dist/styles.css";
-  }
-  head.appendChild(link);
-}
+	link.type = "text/css";
+	link.rel = "stylesheet";
+	if (process.env.NODE_ENV == "production") {
+		link.href =
+			"https://rawcdn.githack.com/joseph-farruggio/Ineject-HTML-Components-with-Script/45f29714f100a4aa1a680a824eb58ec4d2a1971d/dist/styles.css";
+	} else {
+		link.href = "./../dist/styles.css";
+	}
+	head.appendChild(link);
+};
 
 export default injectCSS;
